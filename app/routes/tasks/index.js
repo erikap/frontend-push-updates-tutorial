0fa78079a705c79c81ecaea4e626ebfac521999c
Erika Pauwels <erika.pauwels@gmail.com>
2026-02-27 11:30:01 +0100

    Add cache monitor on tasks index route

--- app/routes/tasks/index.js
+++ app/routes/tasks/index.js
@@ -1,6 +1,8 @@
 import Route from '@ember/routing/route';
 import { service } from '@ember/service';
+import monitorCache from 'ember-polling-push-updates/decorators/monitor-cache'
 
+@monitorCache('/tasks')
 export default class TasksIndexRoute extends Route {
   @service store;
 
